08e37a037567a0486f12748e73d9d10cd374c4a3
calisteveo <soglesbee1@gmail.com>
2014-03-07 20:08:35 -0800

    only moved commented areas did not change code

--- public/trie.js
+++ public/trie.js
@@ -2,6 +2,20 @@ Trie = function(){
   this.characters = {};
 };
 
+  // This function should add the given word,
+  // starting from the given index,
+  // to this Trie.
+
+  // It will be recursive.  It will tell
+  // the correct child of this Trie to learn the word
+  // starting from a later index.
+
+  // Consider what the learn function should do
+  // when it reaches the end of the word?
+  // A word does not necessarily end at a leaf.
+  // You must mark nodes which are the ends of words,
+  // so that the words can be reconstructed later.
+
 Trie.prototype.learn = function(word, index){
   index = index || 0;
   var char = word[index];
@@ -16,26 +30,19 @@ Trie.prototype.learn = function(word, index){
       this.characters[char].learn(word, index+1);
     }
   }
-  // This function should add the given word,
-  // starting from the given index,
-  // to this Trie.
-
-  // It will be recursive.  It will tell
-  // the correct child of this Trie to learn the word
-  // starting from a later index.
-
-  // Consider what the learn function should do
-  // when it reaches the end of the word?
-  // A word does not necessarily end at a leaf.
-  // You must mark nodes which are the ends of words,
-  // so that the words can be reconstructed later.
+ 
 };
+
+  // This function will return all the words which are
+  // contained in this Trie.
+  // it will use currentWord as a prefix,
+  // since a Trie doesn't know about its parents.
 
 Trie.prototype.getWords = function(words, currentWord){
   words = words || [];
   currentWord = currentWord || "";
 
-//for char in this.characters {}
+  //for char in this.characters {}
 
   if (this.isWord){
     words.push(currentWord);
@@ -46,22 +53,21 @@ Trie.prototype.getWords = function(words, currentWord){
       this.characters[char].getWords(words, newWord);
     }
   }
-  return words;
-  // This function will return all the words which are
-  // contained in this Trie.
-  // it will use currentWord as a prefix,
-  // since a Trie doesn't know about its parents.
+  return words; 
 };
 
-Trie.prototype.find = function(word, index){
   // This function will return the node in the trie
   // which corresponds to the end of the passed in word.
-
   // Be sure to consider what happens if the word is not in this Trie.
+
+Trie.prototype.find = function(word, index){
+ 
 };
 
-Trie.prototype.autoComplete = function(prefix){
   // This function will return all completions 
   // for a given prefix.
   // It should use find and getWords.
+
+Trie.prototype.autoComplete = function(prefix){
+  
 };
